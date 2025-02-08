c681c799bd85a0ad6984d7c3ae91bf7645bf3825
Daniel24356 <igwedaniel128@gmail.com>
2025-02-08 13:01:14 -0800

    animations

--- src/Components/Relax/relax.jsx
+++ src/Components/Relax/relax.jsx
@@ -2,19 +2,68 @@ import "./relax.css"
 import profile from "../../assets/profile.png"
 import sign from "../../assets/signature.png"
 import TextProps from "../../Props/TextProps/textprops"
+import CountUp from "react-countup"
+import ScrollTrigger from "react-scroll-trigger"
+import { useEffect, useState } from "react"
 
 const Relax = () => {
+
+    const [counterOn, setCounterOn] = useState(false);
+
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false); 
+    const toRotate = ["1500+","1500+","1500+"];
+    const [text, setText] = useState("");
+    const [delta, setDelta] = useState(300 - Math.random() * 100)
+    const period = 2000;
+  
+    useEffect(() => {
+        let ticker = setInterval(() => {
+           tick();
+        },delta)
+  
+        return () => {clearInterval(ticker)};
+    }, [text])
+  
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0,text.length - 1) : fullText.substring(0, text.length + 1);
+   
+        setText(updatedText);
+        if(isDeleting){
+           setDelta(prevDelta => prevDelta/2)
+        }
+        
+        if(!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        }else if(isDeleting && updatedText === ""){
+            setIsDeleting(fasle);
+            setLoopNum(loopNum + 1);
+            setDelta(2000);
+        }
+    }
+
     return(
         <>
         <section className="relax-sec">
             <div className="relax-div">
+            <ScrollTrigger onEnter={() => setCounterOn(true)} onExit={() => setCounterOn(false)}>
                 <div className="relax-inner">
-                   <div className="relax-suite">
-                       <h1>+76</h1>
+                    
+                    <div className="relax-suite">
+                       <h1>
+                        +
+                        {counterOn &&   <CountUp style={{
+               fontSize: 55
+                }} start={0} end={76} duration={2} delay={0}/>}  
+                       </h1>
                        <p>BIG SUITES</p>
                    </div>
+                   
                 </div>
-           
+                </ScrollTrigger>
             <div>
                 <p className="hotel-txt">FAME PLACE</p>
                 <div className="relax-our">
